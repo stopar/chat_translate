97d27f320561b7ea45fb30c41e85ceaf74480831
Muzaffar <mus.ariff@gmail.com>
2023-08-20 09:47:07 +0800

    fix the bug for error message on teh login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,23 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
-  
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
-  
-  # def switch_locale
+  # The code below will work normally for basic Rails
+  # However for Devise this will not work due to a bug in Warden
+  # For more see: https://github.com/tigrish/devise-i18n
+  #
+  # around_action :switch_locale
+  #
+  # def switch_locale(&action)
   #   locale = params[:locale] || I18n.default_locale
-  #   I18n.with_locale = locale
+  #   I18n.with_locale(locale, &action)
   # end
+  
+  # Instead for devise we will try to use the code below:
+  
+  before_action :set_locale
+  
+  def set_locale
+    locale = params[:locale] || I18n.default_locale
+    I18n.locale = locale
+  end
   
   def default_url_options
     { locale: I18n.locale }
